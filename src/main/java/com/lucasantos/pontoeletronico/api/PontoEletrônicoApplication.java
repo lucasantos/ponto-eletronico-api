40db28362fd3f496b4454d437b2a75f255941784
Lucas Santos <lucasantos10@gmail.com>
2019-03-17 20:27:46 -0300

    Primeiro commit

---
+++ src/main/java/com/lucasantos/pontoeletronico/api/PontoEletrônicoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasantos.pontoeletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletrônicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletrônicoApplication.class, args);
+	}
+
+}
